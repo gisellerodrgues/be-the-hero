4955d362f868d7ac9b393a4f7bb7fc32b66c86a3
Giselle Rodrigues <giselle_rodrigues@outlook.com.br>
2020-03-27 16:26:03 -0300

    Frond-end do app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+
+import Logon from './pages/Logon';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
